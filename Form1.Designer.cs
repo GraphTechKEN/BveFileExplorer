c54b844b34ed3a9e9a03914d3de0dc70150d2d1f
ぐらふ <81954879+GraphTechKEN@users.noreply.github.com>
2022-05-08 21:26:27 +0900

    Add files via upload
    初版 (ベータ版 ver 0.9.0.1)

---
+++ Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace AtsPluginEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnOpenRouteFile = new System.Windows.Forms.Button();
+            this.btnOpenVehicleFile = new System.Windows.Forms.Button();
+            this.btnOpenAtsPluginFile = new System.Windows.Forms.Button();
+            this.btnOpenAtsPluginDirectory = new System.Windows.Forms.Button();
+            this.btnOpenVehicleDirectory = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAtsPluginDirectory = new System.Windows.Forms.Button();
+            this.lblAtsPluginFileName = new System.Windows.Forms.Label();
+            this.btnGenerateRelativePath = new System.Windows.Forms.Button();
+            this.btnHelp = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Font = new System.Drawing.Font("MS UI Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.Location = new System.Drawing.Point(659, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "自動追記";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Font = new System.Drawing.Font("MS UI Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button2.Location = new System.Drawing.Point(751, 11);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(77, 50);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "自動解除";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Enabled = false;
+            this.btnOpen.Font = new System.Drawing.Font("MS UI Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnOpen.Location = new System.Drawing.Point(24, 72);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(101, 50);
+            this.btnOpen.TabIndex = 4;
+            this.btnOpen.Text = "2.路線ファイルを\r\n指定";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnOpenRouteFile
+            // 
+            this.btnOpenRouteFile.Enabled = false;
+            this.btnOpenRouteFile.Location = new System.Drawing.Point(297, 11);
+            this.btnOpenRouteFile.Name = "btnOpenRouteFile";
+            this.btnOpenRouteFile.Size = new System.Drawing.Size(109, 50);
+            this.btnOpenRouteFile.TabIndex = 5;
+            this.btnOpenRouteFile.Text = "路線ファイルを開く";
+            this.btnOpenRouteFile.UseVisualStyleBackColor = true;
+            this.btnOpenRouteFile.Click += new System.EventHandler(this.btnOpenRouteFile_Click);
+            // 
+            // btnOpenVehicleFile
+            // 
+            this.btnOpenVehicleFile.Enabled = false;
+            this.btnOpenVehicleFile.Location = new System.Drawing.Point(412, 11);
+            this.btnOpenVehicleFile.Name = "btnOpenVehicleFile";
+            this.btnOpenVehicleFile.Size = new System.Drawing.Size(108, 50);
+            this.btnOpenVehicleFile.TabIndex = 6;
+            this.btnOpenVehicleFile.Text = "車両ファイルを開く";
+            this.btnOpenVehicleFile.UseVisualStyleBackColor = true;
+            this.btnOpenVehicleFile.Click += new System.EventHandler(this.btnOpenVehicleFile_Click);
+            // 
+            // btnOpenAtsPluginFile
+            // 
+            this.btnOpenAtsPluginFile.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnOpenAtsPluginFile.Enabled = false;
+            this.btnOpenAtsPluginFile.Location = new System.Drawing.Point(527, 11);
+            this.btnOpenAtsPluginFile.Name = "btnOpenAtsPluginFile";
+            this.btnOpenAtsPluginFile.Size = new System.Drawing.Size(104, 50);
+            this.btnOpenAtsPluginFile.TabIndex = 7;
+            this.btnOpenAtsPluginFile.Text = "3.ATSプラグイン\r\nファイルを開く";
+            this.btnOpenAtsPluginFile.UseVisualStyleBackColor = false;
+            this.btnOpenAtsPluginFile.Click += new System.EventHandler(this.btnOpenAtsPluginFile_Click);
+            // 
+            // btnOpenAtsPluginDirectory
+            // 
+            this.btnOpenAtsPluginDirectory.Enabled = false;
+            this.btnOpenAtsPluginDirectory.Location = new System.Drawing.Point(527, 67);
+            this.btnOpenAtsPluginDirectory.Name = "btnOpenAtsPluginDirectory";
+            this.btnOpenAtsPluginDirectory.Size = new System.Drawing.Size(104, 50);
+            this.btnOpenAtsPluginDirectory.TabIndex = 8;
+            this.btnOpenAtsPluginDirectory.Text = "ATSプラグインの\r\n場所を開く";
+            this.btnOpenAtsPluginDirectory.UseVisualStyleBackColor = true;
+            this.btnOpenAtsPluginDirectory.Click += new System.EventHandler(this.btnOpenAtsPluginDirectory_Click);
+            // 
+            // btnOpenVehicleDirectory
+            // 
+            this.btnOpenVehicleDirectory.Enabled = false;
+            this.btnOpenVehicleDirectory.Location = new System.Drawing.Point(412, 67);
+            this.btnOpenVehicleDirectory.Name = "btnOpenVehicleDirectory";
+            this.btnOpenVehicleDirectory.Size = new System.Drawing.Size(107, 49);
+            this.btnOpenVehicleDirectory.TabIndex = 9;
+            this.btnOpenVehicleDirectory.Text = "車両ファイルの\r\n場所を開く";
+            this.btnOpenVehicleDirectory.UseVisualStyleBackColor = true;
+            this.btnOpenVehicleDirectory.Click += new System.EventHandler(this.btnOpenVehicleDirectory_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(157, 17);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 152);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(816, 469);
+            this.textBox1.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(698, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 12);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "↑まだできてません^^;";
+            // 
+            // btnAtsPluginDirectory
+            // 
+            this.btnAtsPluginDirectory.Location = new System.Drawing.Point(24, 11);
+            this.btnAtsPluginDirectory.Name = "btnAtsPluginDirectory";
+            this.btnAtsPluginDirectory.Size = new System.Drawing.Size(101, 55);
+            this.btnAtsPluginDirectory.TabIndex = 13;
+            this.btnAtsPluginDirectory.Text = "1.ATSプラグイン\r\nの場所を指定\r\n";
+            this.btnAtsPluginDirectory.UseVisualStyleBackColor = true;
+            this.btnAtsPluginDirectory.Click += new System.EventHandler(this.btnAtsPluginDirectory_Click);
+            // 
+            // lblAtsPluginFileName
+            // 
+            this.lblAtsPluginFileName.AutoSize = true;
+            this.lblAtsPluginFileName.Location = new System.Drawing.Point(22, 133);
+            this.lblAtsPluginFileName.Name = "lblAtsPluginFileName";
+            this.lblAtsPluginFileName.Size = new System.Drawing.Size(89, 12);
+            this.lblAtsPluginFileName.TabIndex = 14;
+            this.lblAtsPluginFileName.Text = "Target:AtsPlugin";
+            // 
+            // btnGenerateRelativePath
+            // 
+            this.btnGenerateRelativePath.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnGenerateRelativePath.Enabled = false;
+            this.btnGenerateRelativePath.Location = new System.Drawing.Point(659, 67);
+            this.btnGenerateRelativePath.Name = "btnGenerateRelativePath";
+            this.btnGenerateRelativePath.Size = new System.Drawing.Size(169, 49);
+            this.btnGenerateRelativePath.TabIndex = 15;
+            this.btnGenerateRelativePath.Text = "4.ATSプラグインパスを生成";
+            this.btnGenerateRelativePath.UseVisualStyleBackColor = false;
+            this.btnGenerateRelativePath.Click += new System.EventHandler(this.btnGenerateRelativePath_Click);
+            // 
+            // btnHelp
+            // 
+            this.btnHelp.Location = new System.Drawing.Point(700, 122);
+            this.btnHelp.Name = "btnHelp";
+            this.btnHelp.Size = new System.Drawing.Size(86, 24);
+            this.btnHelp.TabIndex = 16;
+            this.btnHelp.Text = "ヘルプ";
+            this.btnHelp.UseVisualStyleBackColor = true;
+            this.btnHelp.Click += new System.EventHandler(this.btnHelp_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(605, 624);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(223, 12);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Copyright ©  2022 Twitter@GraphTechKEN";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(840, 644);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnHelp);
+            this.Controls.Add(this.btnGenerateRelativePath);
+            this.Controls.Add(this.lblAtsPluginFileName);
+            this.Controls.Add(this.btnAtsPluginDirectory);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnOpenVehicleDirectory);
+            this.Controls.Add(this.btnOpenAtsPluginDirectory);
+            this.Controls.Add(this.btnOpenAtsPluginFile);
+            this.Controls.Add(this.btnOpenVehicleFile);
+            this.Controls.Add(this.btnOpenRouteFile);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "AtsPluginEditor ( Beta ver 0.9.0.1 )";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnOpenRouteFile;
+        private System.Windows.Forms.Button btnOpenVehicleFile;
+        private System.Windows.Forms.Button btnOpenAtsPluginFile;
+        private System.Windows.Forms.Button btnOpenAtsPluginDirectory;
+        private System.Windows.Forms.Button btnOpenVehicleDirectory;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAtsPluginDirectory;
+        private System.Windows.Forms.Label lblAtsPluginFileName;
+        private System.Windows.Forms.Button btnGenerateRelativePath;
+        private System.Windows.Forms.Button btnHelp;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,330 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace AtsPluginEditor
+{
+    public partial class Form1 : Form
+    {
+        string systemDrive = Environment.GetEnvironmentVariable("SystemDrive");
+        string currentDirectry = Environment.CurrentDirectory;
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            lblAtsPluginFileName.Text = "";
+        }
+
+        private string strRouteFilePath = "";
+        private string strVehicleFilePath = "";
+        private string strAtsPluginSettingFilePath = "";
+        private string strAtsPluginFilePath = "";
+        private bool flgAtsPluginFileOpen = false;
+        private bool flgAtsPluginDirectoryOpen = false;
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            btnOpenRouteFile.Enabled = false;
+            btnOpenVehicleFile.Enabled = false;
+            btnOpenVehicleDirectory.Enabled = false;
+            btnOpenAtsPluginFile.Enabled = false;
+            btnOpenAtsPluginDirectory.Enabled = false;
+            strRouteFilePath = "";
+            strVehicleFilePath = "";
+            strAtsPluginSettingFilePath = "";
+            flgAtsPluginFileOpen = false;
+            btnGenerateRelativePath.Enabled = false;
+
+            textBox1.Clear();
+            //OpenFileDialogクラスのインスタンスを作成
+            OpenFileDialog ofd2 = new OpenFileDialog();
+            ofd2.Filter = "路線ファイル(*.txt)|*.txt";
+            //ダイアログボックスを閉じる前に現在のディレクトリを復元するようにする
+            ofd2.InitialDirectory = Properties.Settings.Default.RouteFileDirectory;
+            //ofd.RestoreDirectory = true;
+
+            //ダイアログを表示する
+            if (ofd2.ShowDialog() == DialogResult.OK)
+            {
+                Properties.Settings.Default.RouteFileDirectory = Path.GetDirectoryName(ofd2.FileName);
+                //OKボタンがクリックされたとき、選択されたファイルを読み取り専用で開く
+                System.IO.Stream stream;
+                stream = ofd2.OpenFile();
+                strRouteFilePath = ofd2.FileName;
+                btnOpenRouteFile.Enabled = true;
+                if (stream != null)
+                {
+                    //内容を読み込み、表示する
+                    StreamReader sr =
+                        new StreamReader(stream);
+                    string line = "";
+                    int i = 0;
+                    int multiVehicle = 0;
+                    int row = 0;
+                    bool errflg = false;
+                    string path = "";
+                    textBox1.AppendText("路線ファイル：" + strRouteFilePath + "\r\n");
+
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        textBox1.AppendText(line + "\r\n");
+                        line = line.Trim();
+                        if (line.IndexOf(";") < 0 && line.IndexOf("#") < 0) {
+                            if (line.IndexOf("Vehicle") >= 0 && line.IndexOf("VehicleTitle") < 0)
+                            {
+
+                                if (line.Substring(line.IndexOf("=")).Length > 1)
+                                {
+                                    row = i;
+                                    if (line.IndexOf("|") > 0)
+                                    {
+                                        path = line.Substring(0, line.IndexOf("|"));
+                                        path = path.Substring(line.IndexOf("=") + 1);
+                                        multiVehicle++;
+                                    }
+                                    else
+                                    {
+                                        path = line.Substring(line.IndexOf("=") + 1);
+                                    }
+                                }
+                                else
+                                {
+                                    errflg = true;
+                                }
+                            } else if (line.IndexOf("Image") >= 0 )
+                            {
+                                if (line.Substring(line.IndexOf("=")).Length > 1)
+                                {
+                                    row = i;
+                                    string file = line.Substring(line.IndexOf("=") + 1);
+
+                                    pictureBox1.ImageLocation = Path.GetDirectoryName(strRouteFilePath) + "\\" + file.Trim();
+                                }
+                            }
+                        }
+                        i++;
+                    }
+
+                    //閉じる
+                    sr.Close();
+                    stream.Close();
+
+                    if ((row > 0) && !errflg)
+                    {
+                        string dir = Path.GetDirectoryName(strRouteFilePath);
+                        strVehicleFilePath = dir + "\\" + path.Trim();
+                        if (File.Exists(strVehicleFilePath))
+                        {
+                            MessageBox.Show("車両ファイルが見つかりました");
+                            if (multiVehicle > 0)
+                            {
+                                MessageBox.Show("車両ファイルが複数あります、手動で設定してください。データ数：" + multiVehicle);
+                            }
+                            OpenNewVehicleFile(strVehicleFilePath);
+                        }
+                        else
+                        {
+                            MessageBox.Show("車両ファイルが見つかりません。");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("車両ファイルが指定されていません。");
+                    }
+                }
+            }
+        }
+
+        private void OpenNewVehicleFile(string strVehicleFilePath_)
+        {
+            if (File.Exists(strVehicleFilePath_))
+            {
+                btnOpenVehicleFile.Enabled = true;
+                btnOpenVehicleDirectory.Enabled = true;
+                //内容を読み込み、表示する
+                StreamReader sr = new StreamReader(strVehicleFilePath_);
+                string line = "";
+                int i = 0;
+                int row = 0;
+                string path = "";
+                textBox1.AppendText("\r\n");
+                textBox1.AppendText("車両ファイル：" + strVehicleFilePath_ + "\r\n");
+                while ((line = sr.ReadLine()) != null)
+                {
+                    textBox1.AppendText(line+"\r\n");
+                    if (line.IndexOf("ATS") >= 0 || line.IndexOf("Ats") >= 0)
+                    {
+                        row = i;
+
+                        path = line.Substring(line.IndexOf("=") + 1);
+                        /*while (line.IndexOf("..") >=0 ){
+                            line = line.Substring(line.IndexOf("..") + 1);
+                            upDirCount++;
+                        }*/
+
+                    }
+                    i++;
+                }
+                //閉じる
+                sr.Close();
+                //MessageBox.Show(upDirCount.ToString());
+                if (row > 0)
+                {
+                    string dir = Path.GetDirectoryName(strVehicleFilePath_);
+                    strAtsPluginSettingFilePath = Path.GetFullPath(Path.GetDirectoryName(dir + "\\" + path.Trim()) + "\\detailmodules.txt");
+                    if (File.Exists(strAtsPluginSettingFilePath))
+                    {
+                        MessageBox.Show("ATSプラグインが見つかりました");
+                        OpenNewAtsPluginFile(strAtsPluginSettingFilePath);
+                    }
+                    else
+                    {
+                        MessageBox.Show("ATSプラグインが見つからないか、対応していません(DetailModule以外)");
+                    }
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("ATSプラグインが指定されていません");
+            }
+        }
+
+        private void OpenNewAtsPluginFile(string AtsPluginFilePath_)
+        {
+            if (File.Exists(AtsPluginFilePath_))
+            {
+                btnOpenAtsPluginFile.Enabled = true;
+                btnOpenAtsPluginDirectory.Enabled = true;
+                //内容を読み込み、表示する
+                StreamReader sr = new StreamReader(AtsPluginFilePath_);
+                string line = "";
+                int i = 0;
+                int row = 0;
+                string dir = "";
+                textBox1.AppendText("\r\n");
+                textBox1.AppendText("ATSプラグイン：" + AtsPluginFilePath_ + "\r\n");
+                while ((line = sr.ReadLine()) != null)
+                {
+                    textBox1.AppendText(line + "\r\n");
+                    if (line.IndexOf(Path.GetFileName(strAtsPluginFilePath)) >= 0)
+                    {
+                        row = i + 1;
+                    }
+                    i++;
+                }
+                //閉じる
+                sr.Close();
+                if (row > 0)
+                {
+                    MessageBox.Show("既に追記されています。", "SerialOutputプラグインの追加");
+                }
+                else
+                {
+                    MessageBox.Show("ATSプラグインファイルを開き、ATSプラグインパス生成ボタンによりパスを生成し、追記してください。", "SerialOutputプラグインの追加");
+                }
+                flgAtsPluginFileOpen = true;
+                if (flgAtsPluginDirectoryOpen)
+                {
+                    btnGenerateRelativePath.Enabled = true;
+                }
+            }
+            else
+            {
+                MessageBox.Show("ATSプラグインが見つかりません:" + AtsPluginFilePath_);
+            }
+        }
+
+        private void btnOpenRouteFile_Click(object sender, EventArgs e)
+        {
+            Process.Start(strRouteFilePath);
+        }
+
+        private void btnOpenVehicleFile_Click(object sender, EventArgs e)
+        {
+            Process.Start(strVehicleFilePath);
+        }
+
+        private void btnOpenAtsPluginFile_Click(object sender, EventArgs e)
+        {
+            Process.Start(strAtsPluginSettingFilePath);
+        }
+
+        private void btnOpenAtsPluginDirectory_Click(object sender, EventArgs e)
+        {
+            Process.Start(Path.GetDirectoryName(strAtsPluginSettingFilePath));
+        }
+
+        private void btnOpenVehicleDirectory_Click(object sender, EventArgs e)
+        {
+            Process.Start(Path.GetDirectoryName(strVehicleFilePath));
+        }
+
+        private void btnAtsPluginDirectory_Click(object sender, EventArgs e)
+        {
+            //OpenFileDialogクラスのインスタンスを作成
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            ofd.FileName = "";
+            ofd.Filter = "ATSプラグインファイル(*.dll)|*.dll";
+            ofd.Title = "ATSプラグインファイルを選択してください";
+            //ダイアログボックスを閉じる前に現在のディレクトリを復元するようにする
+            ofd.InitialDirectory = Properties.Settings.Default.AtsPluginFileDirectory;
+            //ofd.RestoreDirectory = true;
+
+            //ダイアログを表示する
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                Properties.Settings.Default.AtsPluginFileDirectory = Path.GetDirectoryName(ofd.FileName);
+                strAtsPluginFilePath = ofd.FileName;
+                lblAtsPluginFileName.Text = "ターゲット:" + Path.GetFileName(ofd.FileName);
+                flgAtsPluginDirectoryOpen = true;
+                btnOpen.Enabled = true;
+                if (flgAtsPluginFileOpen)
+                {
+                    btnGenerateRelativePath.Enabled = true;
+                }
+            }
+        }
+
+        private void btnGenerateRelativePath_Click(object sender, EventArgs e)
+        {
+            Uri u1 = new Uri(strAtsPluginSettingFilePath);
+            Uri u2 = new Uri(strAtsPluginFilePath);
+
+            Uri relativeUri = u1.MakeRelativeUri(u2);
+
+            string relativePath = relativeUri.ToString();
+
+            relativePath = relativePath.Replace('/', '\\');
+
+            textBox1.AppendText("\r\n");
+            textBox1.AppendText("ATSプラグインファイル(detailmodules.txtに下記ファイルパスを追記して下さい\r\n");
+            textBox1.AppendText(relativePath);
+        }
+
+        private void btnHelp_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("【説明】\r\n" +
+                "Rock_On様作製のATSプラグイン(DetailManager)専用です\r\n" +
+                "detailmodules.txtを書き換えたいときに使えるかと思います\r\n" +
+                "\r\n" +
+                "【つかいかた】\r\n" +
+                "1.ATSプラグインファイルの場所を指定する" +
+                "2.路線ファイルを指定する\r\n" +
+                "\r\n3.ATSプラグインファイルを開く\r\n" +
+                "4.ATSプラグインファイルパスを生成し、追記する\r\r" +
+                "\r\n" + 
+                "【できないこと】\r\n" +
+                "車両ファイルを複数している路線データ、DetailManager非対応のプラグイン、自動追加削除機能\r\n" + 
+                "\r\n" + 
+                "【注意事項】\r\n※動作保証なし、自己責任かつ個人使用でお願いします※", "使い方");
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.Save();
+        }
+    }
+}
